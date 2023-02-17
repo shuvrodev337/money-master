0c972a8f147327138df42de8024304c015515efc
unknown <shuvrodevmondal337@gmail.com>
2023-02-17 06:32:40 +0600

    made validation more better

--- js/money.js
+++ js/money.js
@@ -2,11 +2,10 @@
 function getInputValueByID(elementID) {
   const inputElement = document.getElementById(elementID);
   const inputElementString = inputElement.value;
+  const inputValue = parseFloat(inputElementString);
   if (isNaN(inputElementString)) {
-    alert("please type carefully")
     return
   }
-  const inputValue = parseFloat(inputElementString);
   return inputValue;
 }
 
@@ -28,13 +27,17 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   const clothesExpense = getInputValueByID("clothes-expense");
   const totalExpenses = foodExpense + rentExpense + clothesExpense;
   if (totalExpenses > income) {
-    alert("Cut your coat according to your cloth");
+    alert("Your expense is more than you income");
     return;
   }
    else if (isNaN(totalExpenses)) {
-    // alert("please fill up all boxes with valid numbers");
+    alert("Please fill up all the boxes with valid numbers!!!");
     return;
+  }else if (income<0 || foodExpense<0 || rentExpense<0 || clothesExpense<0) {
+    alert("Insert positive numbers!!!")
+    return
   }
+
   const balanceBeforeSaving = income - totalExpenses;
   setTextElementValueByID("total-expenses", totalExpenses);
   setTextElementValueByID("balance-before-saving", balanceBeforeSaving);
@@ -55,14 +58,22 @@ document.getElementById("savings-button").addEventListener("click", function () 
     const balanceBeforeSavingString = getTextElementValueByID("balance-before-saving");
     const balanceBeforeSaving = parseFloat(balanceBeforeSavingString);
     const remainingBalance = balanceBeforeSaving - savingAmount;
-    if (remainingBalance < 0) {
-      alert("Not Enough money to save");
-      return;
-    }
-     else if (isNaN(savingAmount)) {
-    //   alert("pls enter valid numbers");
+
+    if (savingsPercentage>100) {
+        alert("Can not save more than 100%");
+        return
+    }else if (savingsPercentage<0) {
+        alert("Can not save negative percentage!!!");
+        return
+    }else if (isNaN(savingAmount)) {
+        alert("please insert a valid percentage!!!")
+        return;
+    } else if (remainingBalance < 0) {
+      alert("Not Enough balance left to save");
       return;
     }
     setTextElementValueByID("saving-amount", savingAmount);
     setTextElementValueByID("remaining-balance", remainingBalance);
   });
+
+// console.log( parseFloat('o9kk'));
